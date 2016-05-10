5ef5f31d1f581ad63e50e92b07b55b7607a8e9d0
vinoscript <wstikk@gmail.com>
2016-05-10 23:33:31 +0000

    writing urls to csv - still need to format

--- index.js
+++ index.js
@@ -16,31 +16,41 @@ function getUrlPath(path){
       $('a').each(function(){
         var url = $(this).attr('href');
 
-        var filePattern = new RegExp(/.*(\.[a-z]{3})$/);
-        var dotDotPattern = new RegExp(/.*\.\..*/);
-        var urlPattern = new RegExp(/^http.*/);
-        var endsWithSlash = new RegExp(/\/$/);
+        var filePattern     =   new RegExp(/.*(\.[a-z]{3})$/);
+        var dotDotPattern   =   new RegExp(/.*\.\..*/);
+        var urlPattern      =   new RegExp(/^http.*/);
+        var endsWithSlash   =   new RegExp(/\/$/);
 
         if( filePattern.test(url) ){
           urls.push(url);
-          console.log(url);
+          // console.log(url);
+
+          fs.writeFile("images.csv", urls, function(err) {
+              if(err) {
+                return console.log(err);
+              }
+              console.log("The file was saved!");
+          }); 
+
         } 
 
         else if ( endsWithSlash.test(url) && !(urlPattern.test(url)) && !(dotDotPattern.test(url)) ) {
           getUrlPath(pathPrefix + url);
         }
-
-        else {
-        }
-
+      
       });
 
     }
+
     else {
       console.log("failed to get data");
     }
+
   })
 }
 
+
+
 getUrlPath(pathMain);
+
 
