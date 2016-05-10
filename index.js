b2ac72d8846626aaf350e9a7e1a576debd8239ee
vinoscript <wstikk@gmail.com>
2016-05-10 22:40:31 +0000

    successfully extracting file path urls only

--- index.js
+++ index.js
@@ -2,29 +2,45 @@ var fs = require('fs');
 var cheerio = require('cheerio');
 var request = require('request');
 
-var path = 'http://substack.net/images/';
-// var htmlData = '';
+var pathMain = 'http://substack.net/images/';
+var pathPrefix = 'http://substack.net';
 var urls = [];
-// var url;
 
-request(path, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log("succesfully connected to data"); 
-    var $ = cheerio.load(body);
-    $('a').each(function(){
-      // var url = this.attr('href');
-      urls.push($(this).attr('href'));
-    });
-    console.log(urls);
+function getUrlPath(path){ 
+  request(path, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
 
-  }
-  else {
-    console.log("failed to get data");
-  }
-})
+      console.log("succesfully connected to data"); 
+      var $ = cheerio.load(body);
 
+      $('a').each(function(){
+        var url = $(this).attr('href');
 
-// var cheerio = require('cheerio'),
-//     $ = cheerio.load(htmlData);
+        var filePattern = new RegExp(/.*(\.[a-z]{3})$/);
+        var dotDotPattern = new RegExp(/.*\.\..*/);
+        var urlPattern = new RegExp(/^http.*/);
+        var endsWithSlash = new RegExp(/\/$/);
 
+        if( filePattern.test(url) ){
+          urls.push(url);
+          console.log(url);
+        } 
+
+        else if ( endsWithSlash.test(url) && !(urlPattern.test(url)) && !(dotDotPattern.test(url)) ) {
+          getUrlPath(pathPrefix + url);
+        }
+
+        else {
+        }
+
+      });
+
+    }
+    else {
+      console.log("failed to get data");
+    }
+  })
+}
+
+getUrlPath(pathMain);
 
